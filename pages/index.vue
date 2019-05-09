4c3f8fdd4dedbdd39f0c68f9b60e4e90087224ca
Michelle <kiecoomi@gmail.com>
2019-05-09 21:54:10 +0800

    UPDATE 地圖頁shops：改由props傳入by data in index.vue

--- pages/index.vue
+++ pages/index.vue
@@ -2,8 +2,8 @@
   <div>
     <Navbar :cities="cities" :typeOptions="items"/>
     <b-container class='map-container'>
-      <ShopList  />
-      <b-row>  
+      <ShopList  :selectedShops="data"/>
+      <b-row>
         <b-col lg='12' class='map'>
           <no-ssr>
             <gmap-map
@@ -36,7 +36,7 @@
                     <p style="margin-top: 10px;font-size: 14px;">
                       類別
                       <span style="float:right;color:#4de680;font-weight:bold;">休息中</span>
-                    </p>  
+                    </p>
                   </div>
                 </div>
 
@@ -45,7 +45,7 @@
           </no-ssr>
         </b-col>
       </b-row>
-      
+
     </b-container>
   </div>
 </template>
@@ -177,6 +177,7 @@ export default {
   },
   data() {
     return {
+
       hideSidebar: true,
       infoWindow: {
         options: {
@@ -262,6 +263,61 @@ export default {
         {value: '', text: "南投市"},
         {value: '', text: "澎湖市"},
         {value: '', text: "金門市"},
+      ],
+      data: [
+        {
+          "address" : "台中市中區三民路二段18巷6號",
+          "business_time" : "請注意粉絲頁",
+          "latitude" : 24.1422103,
+          "longitude" : 120.6589805,
+          "name" : "七喜廚房",
+          "phone" : "04-22211700",
+          "remark" : "七喜廚房又叫明日餐桌，是第一個在台灣實踐剩食利用的經營空間，嘗試開放共食、堅持以社區付的起的價格，推動在地市場的剩食，在地消耗。他們透過明日餐桌計畫，推動社區廚房減少食材過度浪費的問題。被人類拋棄的醜蔬果及剩餘食材，在這裡變身為一道道新鮮健康佳餚，來吧，走進這裡享受盛食，體會食材的美味，實踐真正的飲食零浪費。",
+          "type" : "food_share",
+          "url" : "https://www.facebook.com/7upkitchen/\nhttps://www.7ckitchen.com",
+          "logo": 'https://i.imgur.com/OiSniLG.jpg',
+          "stack":  [ 'tag1_Unpackaged', 'tag2_ZeroWaste', 'tag3', 'tag4' ]
+        },
+        {
+          "address" : "108台北市萬華區中華路二段334號",
+          "business_time" : "每週四1130~1300市場惜食分享, 每週五1500~1800共煮送餐交流",
+          "latitude" : 25.0989537,
+          "longitude" : 121.5081786,
+          "name" : "南機拌飯",
+          "phone" : "0963 615 016",
+          "remark" : "南機拌飯由「地下勞動合作社」經營，作為地方社群工作、社會行動實驗、社區生活交流的活動空間。共同分租的夥伴團體還有「人生百味公司」與「夢想城鄉協會」等。萬華南機場老社區的神秘地下室，正聚集一群人交流合作：經營社群基地與協力社區發展，實驗社區廚房與在地市集；再生舊物與剩食，分享勞動與理念。",
+          "type" : "free_shop",
+          "url" : "https://www.facebook.com/NanjiRice/",
+          "logo": 'https://i.imgur.com/YGKTNkA.jpg',
+          "stack":  [ 'tag1_Unpackaged', 'tag2_ZeroWaste', 'tag3' ]
+        },
+        {
+          "address" : "950台東縣台東市漢陽北路169巷64弄26號",
+          "business_time" : "",
+          "latitude" : 22.74739,
+          "longitude" : 121.132759,
+          "name" : "迷走。回家旅人空間",
+          "phone" : 989360055,
+          "remark" : "迷走是生活的旅行，回家是生命的感動。台東市區友善環境旅人空間。綠色慢活Ｘ深度旅遊Ｘ共享冰箱。Eco-friendly traveler's inn in Taitung city.Slow life, in-depth travel, shared fridge.",
+          "shop_type" : "",
+          "type" : "food_share",
+          "url" : "https://www.facebook.com/lostandfoundtaitung/\nhttps://jtwagor.wixsite.com/lostandfoundtaitung",
+          "logo": 'https://i.imgur.com/OiSniLG.jpg',
+          "stack":  [ 'tag1_Unpackaged', 'tag2_ZeroWaste' ]
+        },
+        {
+          "address" : "300新竹市東區光復路二段101號清華大學風雲樓一樓水木書苑內",
+          "business_time" : "週二~周日1000~1800",
+          "latitude" : 24.792323,
+          "longitude" : 120.9950025,
+          "name" : "水木咖啡 * 小客廳",
+          "phone" : "03 572 3474",
+          "remark" : "咖啡、書籍、食農教育",
+          "type" : "",
+          "url" : "https://www.facebook.com/%E6%B0%B4%E6%9C%A8%E5%92%96%E5%95%A1-x-%E5%B0%8F%E5%AE%A2%E5%BB%B3-The-Living-Room-645497232164723/",
+          "logo": 'https://i.imgur.com/YGKTNkA.jpg',
+          "stack":  [ 'tag1_Unpackaged' ]
+        },
       ]
     }
   },
