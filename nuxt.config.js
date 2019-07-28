7f7383061bb2cd15719318f03c40ac1b28344728
Yumi Chen <monorhapsody@gmail.com>
2019-07-29 02:04:54 +0800

    add ul margin-bottom: 0 assets/style/app.styl
    add favicon, modify title
    nuxt.config.js
    static/favicon.ico
    
    add build files
    dist/200.html
    dist/_nuxt/4f1018498bc6a9888bfb.js
    dist/favicon.ico
    dist/index-map/index.html
    dist/index.html
    dist/inspire/index.html
    dist/map/index.html
    dist/recommendations/index.html
    dist/shop-map/index.html
    dist/shop/index.html

--- nuxt.config.js
+++ nuxt.config.js
@@ -9,7 +9,7 @@ module.exports = {
   ** Headers of the page
   */
   head: {
-    title: pkg.name,
+    title: "台灣零廢棄地圖",
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
