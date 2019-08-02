b95837297ed619e52add9c574c87dbe2227b7b65
Yumi Chen <monorhapsody@gmail.com>
2019-08-03 01:16:57 +0800

    add messenger popup function components/Footer.vue
    add build files
    dist/200.html
    dist/_nuxt/20306a6e00c2f1f94749.js
    dist/_nuxt/2b8286f86bf162bcacde.js
    dist/_nuxt/54300b3854f263321fa4.js
    dist/_nuxt/bd83b683ab9b0ae87582.js -> dist/_nuxt/6f044072515f972b3c99.js
    dist/_nuxt/7b4906b96922f3e4d9fe.js
    dist/_nuxt/7b96a59f8b82e6900a82.js
    dist/_nuxt/8fb8ecc0b1e48f1714e6.js
    dist/_nuxt/LICENSES
    dist/_nuxt/a65cc948434f2a98dc2e.js
    dist/_nuxt/790795a9d38eee01e8eb.js -> dist/_nuxt/b0dd29eccc41edf8cbf2.js
    dist/_nuxt/b98ca14cf02ae624777d.js
    dist/_nuxt/d88959de36acac33a138.js
    dist/_nuxt/b708394f1f8343a1162c.js -> dist/_nuxt/da324db81bbe14b63702.js
    dist/_nuxt/e84bc7917a02ac1e0734.js
    dist/_nuxt/img/8daeddc.svg -> dist/_nuxt/img/3009096.svg
    dist/index-map/index.html
    dist/index.html
    dist/inspire/index.html
    dist/map/index.html
    dist/recommendations/index.html
    dist/shop-map/index.html
    dist/shop/index.html

---
+++ _nuxt/8fb8ecc0b1e48f1714e6.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[7],{831:function(e,n,t){"use strict";t.r(n);var c={components:{MapPage:t(773).a}},l=t(12),component=Object(l.a)(c,function(){var e=this.$createElement;return(this._self._c||e)("MapPage")},[],!1,null,null,null);n.default=component.exports}}]);
