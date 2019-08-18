45c800e588c6b93cd15ac417969f3fc7441be46f
Yumi Chen <monorhapsody@gmail.com>
2019-08-19 02:46:13 +0800

    add build files dist/200.html dist/_nuxt/02e9edc890d34c4497a1.js dist/_nuxt/034583095a07f42fe97f.js dist/_nuxt/241e4ea92a5f4f93b5cb.js dist/_nuxt/296f21d3d152e177bbf5.js dist/_nuxt/596a4ad54f56ac800753.js dist/_nuxt/7afdb01f0d62c7ac4b15.js -> dist/_nuxt/6f8ed1f941b1cd616c8d.js dist/_nuxt/9788b306826d245b4b50.js dist/_nuxt/ba676e2930a68d3cab49.js dist/_nuxt/dbaa0f329f74156224ea.js -> dist/_nuxt/be9eb08019bef4cc890e.js dist/_nuxt/cb38cfc3892cb6a45450.js dist/_nuxt/d5df222a9fd7b6b3d87b.js dist/_nuxt/e8f85173455ba0c6e2e5.js dist/_nuxt/f0ac527e9c7558cce23a.js dist/_nuxt/f14ed23475c4e7fc0223.js dist/_nuxt/f773e28340f25d380552.js dist/_nuxt/img/386f941.svg dist/_nuxt/img/dc12b48.svg -> dist/_nuxt/img/595b0c2.svg dist/_nuxt/img/b2977de.svg -> dist/_nuxt/img/78ee583.svg dist/about/index.html dist/index-map/index.html dist/index.html dist/inspire/index.html dist/map/index.html dist/recommendations/index.html dist/shop-map/index.html dist/shop/index.html

---
+++ dist/_nuxt/241e4ea92a5f4f93b5cb.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[9],{853:function(e,n,t){"use strict";t.r(n);var c={components:{MapPage:t(784).a}},l=t(12),component=Object(l.a)(c,function(){var e=this.$createElement;return(this._self._c||e)("MapPage")},[],!1,null,null,null);n.default=component.exports}}]);
