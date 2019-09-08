5fb3761884ea760fbe0a2b313d866e238df2bbf2
Yumi Chen <monorhapsody@gmail.com>
2019-09-08 22:11:19 +0800

    add build files dist/200.html dist/_nuxt/34cfa3d8f0528cfe2d7f.js dist/_nuxt/53c36f12e223941b309f.js -> dist/_nuxt/37f02caf89f0070eeca1.js dist/_nuxt/57382b53d13183294116.js dist/_nuxt/7575e963816a2acf8f89.js dist/_nuxt/779d7e7aabf06a2e8afe.js dist/_nuxt/b9f1b7c13352fd5fd55e.js -> dist/_nuxt/854c68f4c707b7cf0a5b.js dist/_nuxt/a59f7e16c1532e03bc8c.js dist/_nuxt/afb3e4c59fdc7ce45d9d.js dist/_nuxt/025f318d8d832eca0b90.js -> dist/_nuxt/b6c8296e0b8bdac1134c.js dist/_nuxt/be24e3d4eb1036a345a4.js dist/_nuxt/c4142d6717a880280ee0.js dist/_nuxt/eb6f61f86c1dd9055887.js dist/_nuxt/ee1df2b20f32e3c7ff63.js dist/_nuxt/ef5ab82690351b6bee16.js dist/about/index.html dist/index-map/index.html dist/index.html dist/inspire/index.html dist/map/index.html dist/recommendations/index.html dist/shop-map/index.html dist/shop/index.html

--- _nuxt/f82f1d36793cda06d693.js
+++
@@ -1,1 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[8],{863:function(e,n,t){"use strict";t.r(n);var c={components:{MapPage:t(788).a}},l=t(12),component=Object(l.a)(c,function(){var e=this.$createElement;return(this._self._c||e)("MapPage")},[],!1,null,null,null);n.default=component.exports}}]);
